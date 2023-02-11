018e847f98f1d122f8e0abebc187fd0c2631d418
vava <febratamaa@gmail.com>
2023-02-11 10:27:25 +0700

    membuat tampilan dashboard admin

--- index.php
+++ index.php
@@ -24,46 +24,83 @@ if (isset($_POST["cari"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Belajar Databases</title>
+    <title>Dashboard</title>
+
+    <!-- css bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+
 </head>
 
 <body>
-    <h1>Halaman Admin</h1>
-    <a href="tambah.php">Tambah data Mahasiswa</a>
-    <a style="margin-left: 15px;" href="logout.php">Logout!</a>
-    <br><br>
+    <!-- navbar -->
+    <nav class="navbar bg-primary navbar-dark">
+        <div class="container">
+            <a class="navbar-brand fs-1">Dashboard Admin</a>
+            <form class="d-flex">
+                <a href="logout.php">
+                    <button class="btn btn-light fw-semibold" type="submit">Logout</button>
+                </a>
+            </form>
+        </div>
+    </nav>
+    <!-- akhir navbar -->
 
-    <form action="" method="post">
-        <input type="text" name="search" size="30" autocomplete="off" placeholder="Masukkan keyword untuk pencarian..">
-        <button type="submit" name="cari">Cari!</button>
-    </form>
-    <br>
-
-    <table border="1" cellpadding="10" cellspacing="0">
-        <tr>
-            <th>No.</th>
-            <th>Aksi</th>
-            <th>NPM</th>
-            <th>Nama</th>
-            <th>Email</th>
-            <th>Jurusan</th>
-        </tr>
-        <?php $i = 1; ?>
-        <?php foreach ($mahasiswa as $mhs) : ?>
-            <tr>
-                <td><?php echo $i; ?></td>
-                <td>
-                    <a href="ubah.php?id=<?php echo $mhs["id"]; ?>">Ubah</a> |
-                    <a href="hapus.php?id=<?php echo $mhs["id"]; ?>" onclick="return confirm('Yakin menghapus data?');">Hapus</a>
-                </td>
-                <td><?php echo $mhs["npm"]; ?></td>
-                <td><?php echo $mhs["nama"]; ?></td>
-                <td><?php echo $mhs["email"]; ?></td>
-                <td><?php echo $mhs["jurusan"]; ?></td>
-            </tr>
-            <?php $i++; ?>
-        <?php endforeach; ?>
-    </table>
+    <!-- tabel data -->
+    <div class="container">
+        <div class="row">
+            <div class="col-6 my-3">
+                <a href="tambah.php">
+                    <button class="btn btn-success fw-semibold">Tambah data Mahasiswa</button>
+                </a>
+            </div>
+            <div class="col-6 my-3">
+                <form class="d-flex" role="search" action="" method="post">
+                    <input class="form-control me-2" type="text" name="search" placeholder="Masukkan keyword untuk pencarian.." aria-label="Search" autocomplete="off">
+                    <button class="btn btn-outline-success fw-semibold" type="submit" name="cari">Cari!</button>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center">
+                <table class="table">
+                    <thead class="table-primary">
+                        <th>No.</th>
+                        <th>Aksi</th>
+                        <th>NPM</th>
+                        <th>Nama</th>
+                        <th>Email</th>
+                        <th>Jurusan</th>
+                    </thead>
+                    <?php $i = 1; ?>
+                    <?php foreach ($mahasiswa as $mhs) : ?>
+                        <tbody>
+                            <td><?php echo $i; ?></td>
+                            <td>
+                                <a href="ubah.php?id=<?php echo $mhs["id"]; ?>">
+                                    <button class="btn btn-info btn-sm text-white fw-semibold" type="submit" name="cari">Ubah</button>
+                                </a>
+                                <a href="hapus.php?id=<?php echo $mhs["id"]; ?>" onclick="return confirm('Yakin menghapus data?');">
+                                    <button class="btn btn-danger btn-sm text-white fw-semibold ms-1" type="submit" name="cari">Hapus</button>
+                                </a>
+                            </td>
+                            <td><?php echo $mhs["npm"]; ?></td>
+                            <td><?php echo $mhs["nama"]; ?></td>
+                            <td><?php echo $mhs["email"]; ?></td>
+                            <td><?php echo $mhs["jurusan"]; ?></td>
+                        </tbody>
+                        <?php $i++; ?>
+                    <?php endforeach; ?>
+                </table>
+            </div>
+        </div>
+    </div>
+    <!-- akhir tabel -->
 </body>
 
 </html>
